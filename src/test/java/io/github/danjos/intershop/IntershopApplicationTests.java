d4bb919664d9fa75cf1fd9124781e8c874fbfef3
Данни Титуанья <davila.tituanya@gazprombank.ru>
2025-06-20 23:20:45 +0300

    Add initial files

---
+++ src/test/java/io/github/danjos/intershop/IntershopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.danjos.intershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

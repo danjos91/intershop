f5563871f9d981bd80d10939a2b19bc93fbaac88
Данни Титуанья <davila.tituanya@gazprombank.ru>
2025-07-08 21:50:52 +0300

    Add anotations on model

--- src/main/java/io/github/danjos/intershop/model/Order.java
+++ src/main/java/io/github/danjos/intershop/model/Order.java
@@ -1,13 +1,22 @@
 package io.github.danjos.intershop.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Entity
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public long id;
+
+    @Column(name = "user_id")
+    public long userId;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     public List<Item> items;
 
 }
--- src/main/java/io/github/danjos/intershop/model/User.java
+++ src/main/java/io/github/danjos/intershop/model/User.java
@@ -3,9 +3,12 @@ package io.github.danjos.intershop.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Entity
-public class Item {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,6 +17,7 @@ public class Item {
     @Column
     private String name;
 
-    @Column
-    private String description;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders = new ArrayList<>();
+
 }

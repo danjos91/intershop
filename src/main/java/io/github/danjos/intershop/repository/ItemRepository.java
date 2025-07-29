7e25292e3766ef7dcaddae279fc005a0e756f825
Данни Титуанья <davila.tituanya@gazprombank.ru>
2025-07-30 01:25:06 +0300

    Delete unnecesary methods and import

--- src/main/java/io/github/danjos/intershop/repository/ItemRepository.java
+++ src/main/java/io/github/danjos/intershop/repository/ItemRepository.java
@@ -6,16 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    List<Item> findByTitleContainingIgnoreCase(String title);
-    List<Item> findByOrderByTitleAsc();
-    List<Item> findByOrderByPriceAsc();
-
     Page<Item> findByTitleContainingIgnoreCase(String title, Pageable pageable);
     Page<Item> findByOrderByTitleAsc(Pageable pageable);
     Page<Item> findByOrderByPriceAsc(Pageable pageable);
-
 }

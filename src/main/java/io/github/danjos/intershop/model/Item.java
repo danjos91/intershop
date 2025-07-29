2ae71ada0d28e3992ec8fb4b9a8517106b6a0398
Данни Титуанья <davila.tituanya@gazprombank.ru>
2025-07-29 10:32:55 +0300

    Fix schema bug

--- src/main/java/io/github/danjos/intershop/model/Item.java
+++ src/main/java/io/github/danjos/intershop/model/Item.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Table(name = "items")
 @Data
 public class Item {
     @Id
--- src/main/java/io/github/danjos/intershop/model/OrderItem.java
+++ src/main/java/io/github/danjos/intershop/model/OrderItem.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Table(name = "order_items")
 @Data
 public class OrderItem {
     @Id
